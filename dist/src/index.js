292c374da315a5ec70a32ebc98f63e61616a5966
Fabian <fabian@aimermedia.com>
2023-04-05 21:20:24 +0930

    changed cors policy

--- dist/src/index.js
+++ dist/src/index.js
@@ -4,14 +4,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const questions_json_1 = __importDefault(require("../data/questions.json"));
+const sections_json_1 = __importDefault(require("../data/sections.json"));
 const app = (0, express_1.default)();
 const port = 3001;
+// TODO change this behaviour later. This is not recommended for production environments, as it may pose
+// security risks. It's better to restrict the origins that can access your API by specifying them explicitly.
+app.use((_req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
 app.get("/", (req, res) => {
     res.send("Hello World!!");
 });
-app.get("/getQuestions", (req, res) => {
-    res.json(questions_json_1.default);
+app.get("/quizSections", (req, res) => {
+    console.log("calling /quizSections");
+    res.json(sections_json_1.default);
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
 import express from "express";
-import questionData from "../data/questions.json";
+import sectionData from "../data/sections.json";
+import testData from "../data/testData.json";
 
 const app = express();
 const port = 3001;
+
+// TODO change this behaviour later. This is not recommended for production environments, as it may pose
+// security risks. It's better to restrict the origins that can access your API by specifying them explicitly.
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!!");
 });
 
-app.get("/getQuestions", (req, res) => {
-  res.json(questionData);
+app.get("/quizSections", (req, res) => {
+  console.log("calling /quizSections");
+  res.json(sectionData);
 });
 
 app.listen(port, () => {
