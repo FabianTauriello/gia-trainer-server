efd866f1b6d373100b4a4a220a78c9cd26f93a2f
Fabian <fabian@aimermedia.com>
2023-06-13 00:25:57 +0100

    updated sql statements

--- sql/LocalhostConnection.session.sql
+++ sql/LocalhostConnection.session.sql
@@ -1,12 +1,12 @@
--- ------------------------------------- USER QUERIES -------------------------------------
+-- ------------------------------------- USER TABLE QUERIES -------------------------------------
 
 -- @block
 CREATE TABLE user (
    id INT NOT NULL AUTO_INCREMENT,
    email VARCHAR(255) NOT NULL,
    password VARCHAR(255) NOT NULL,
-   first_name VARCHAR(255) NOT NULL,
-   last_name VARCHAR(255) NOT NULL,
+   firstName VARCHAR(255) NOT NULL,
+   lastName VARCHAR(255) NOT NULL,
    PRIMARY KEY (id)
 );
 
@@ -44,11 +44,13 @@ ADD password VARCHAR(255) NOT NULL;
 ALTER TABLE user
 RENAME COLUMN last_name TO lastName;
 
--- ------------------------------------- QUIZ ATTEMPT QUERIES -------------------------------------
+-- ------------------------------------- QUIZ ATTEMPT TABLE QUERIES -------------------------------------
 
 -- @block
 CREATE TABLE quizAttempt (
    id INT NOT NULL AUTO_INCREMENT,
+   userId INT NOT NULL,
+   totalScore VARCHAR(25) NOT NULL,
    FOREIGN KEY (userId) REFERENCES user(id),
    PRIMARY KEY (id)
 );
@@ -62,6 +64,28 @@ DROP TABLE quizAttempt;
 -- @block
 ALTER TABLE quizAttempt
 DROP FOREIGN KEY userId;
+
+
+-- ------------------------------------- ANSWER TABLE QUERIES -------------------------------------
+
+-- @block
+CREATE TABLE answer (
+   id INT NOT NULL AUTO_INCREMENT,
+   quizAttemptId INT NOT NULL,
+   question VARCHAR(1000) NOT NULL,
+   FOREIGN KEY (quizAttemptId) REFERENCES quizAttempt(id),
+   PRIMARY KEY (id)
+);
+
+-- @block
+SELECT * FROM answer;
+
+-- @block
+DROP TABLE answer;
+
+-- @block
+ALTER TABLE answer
+DROP FOREIGN KEY quizAttemptId;
 
 -- ------------------------------------- GENERAL QUERIES -------------------------------------
 
