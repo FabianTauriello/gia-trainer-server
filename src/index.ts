a808b42d3c455abdfb35f23621a4e427340c620c
Fabian T <ftauriello@hotmail.com>
2023-06-22 13:02:10 +0100

    added credentials header

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,7 @@ app.use((_req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
