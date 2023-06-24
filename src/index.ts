fcafc2efcc07b01a1cb1695c05e04182aa74e874
Fabian T <ftauriello@hotmail.com>
2023-06-24 16:53:28 +0100

    update index to use cors for dev mode only

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import "dotenv/config";
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
 import questionData from "../data/questions.json";
 import fs from "fs";
 import path from "path";
 import https from "https";
 import cors from "cors";
-import morgan from "morgan";
 import { ApiResponse, Question } from "./domain/Types.js";
 import { Auth } from "./services/Auth.js";
 import { Utils } from "./utils/Utils.js";
@@ -13,32 +12,18 @@ import { Quiz } from "./services/Quiz.js";
 
 const app = express();
 
-// Configure morgan as middleware for logging
-app.use(morgan("combined"));
-
-// Error handling middleware
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err);
-  res.status(500).json({ error: "Internal Server Error" });
-});
-
 app.use(express.json());
-// TODO change this behaviour later. This is not recommended for production environments, as it may pose
-// security risks. It's better to restrict the origins that can access your API by specifying them explicitly.
-// app.use((_req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "https://localhost:5173/");
-//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   next();
-// });
-app.use(
-  cors({
-    origin: ["*"],
-    methods: ["GET", "PUT", "POST", "DELETE"],
-    allowedHeaders: ["Origin", "Content-Type", "Authorization"],
-  })
-);
+
+// Enable CORS only during development
+if (process.env.ENV === "dev") {
+  app.use(
+    cors({
+      origin: "*",
+      methods: ["GET", "PUT", "POST", "DELETE"],
+      allowedHeaders: ["Origin", "Content-Type", "Authorization"],
+    })
+  );
+}
 
 const httpPort = 3001;
 const httpsPort = 3002;
@@ -59,15 +44,6 @@ httpsServer.listen(httpsPort, () => {
   console.log(`HTTPS gia-trainer-server listening on port ${httpsPort}`);
 });
 
-// app.get("/.well-known/pki-validation/5AC11FE6F957A94D5B49AF1925882B3B.txt", (req, res) => {
-//   const filePath = "assets/5AC11FE6F957A94D5B49AF1925882B3B.txt";
-//   const fileContent = fs.readFileSync(filePath, "utf8");
-
-//   res.setHeader("Content-Type", "text/plain");
-
-//   res.send(fileContent);
-// });
-
 app.get("/", (req, res) => {
   res.send("Hello World!!");
 });
@@ -85,6 +61,7 @@ app.post("/signUp", async (req, res) => {
 });
 
 app.get("/quizQuestions", (req, res) => {
+  console.log("calling");
   // TODO kinda ugly and missing trycatch
   const numberedQuestions: Question[] = questionData.map((q: any, i: any) => ({ ...q, number: i }));
   const sortedQuestions = Utils.sortQuestionsByCategory(numberedQuestions);
